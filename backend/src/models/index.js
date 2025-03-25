b5146798264b48b6f64f0945de6f00b5141be54e
kristinaradeva1 <kriradeva@abv.bg>
2025-03-25 17:44:30 +0200

    wrote the index file

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -1,1 +1,19 @@
-g
+const sequelize = require('../config/database'); // Взима конфигурацията на базата
+const User = require('./user'); // Импортира модела User
+const Project = require('./project'); // Импортира модела Project
+const Issue = require('./issue'); // Импортира модела Issue
+const Note = require('./note'); // Импортира модела Note
+
+User.hasMany(Issue, { foreignKey: 'assignedTo', as: 'AssignedIssues' });
+User.hasMany(Issue, { foreignKey: 'assignedBy', as: 'CreatedIssues' });
+Project.hasMany(Issue, { foreignKey: 'projectId', as: 'ProjectIssues' });
+Issue.hasMany(Note, { foreignKey: 'issueId', as: 'IssueNotes' });
+User.hasMany(Note, { foreignKey: 'userId', as: 'UserNotes' });
+
+Issue.belongsTo(User, { foreignKey: 'assignedTo', as: 'AssignedUser' });
+Issue.belongsTo(User, { foreignKey: 'assignedBy', as: 'CreatorUser' });
+Issue.belongsTo(Project, { foreignKey: 'projectId', as: 'Project' });
+Note.belongsTo(Issue, { foreignKey: 'issueId', as: 'Issue' });
+Note.belongsTo(User, { foreignKey: 'userId', as: 'Author' });
+
+module.exports = { sequelize, User, Project, Issue, Note };
