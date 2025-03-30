169d18a10ff332a477ce7adeec6e5db0ace43b21
kristinaradeva1 <kriradeva@abv.bg>
2025-03-30 21:14:39 +0300

    Remove the length constraint in the password in the user table

--- models/user.js
+++ models/user.js
@@ -3,16 +3,22 @@ const sequelize = require('../config/database');
 
 const User = sequelize.define('User', {
     id: {
-        type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
     },
     username: {
-        type: DataTypes.STRING(20), allowNull: false
+        type: DataTypes.STRING(20),
+        allowNull: false
     },
     email: {
-        type: DataTypes.STRING(30), allowNull: false, unique: true
+        type: DataTypes.STRING(30),
+        allowNull: false,
+        unique: true
     },
     password: {
-        type: DataTypes.STRING(20), allowNull: false
+        type: DataTypes.STRING,
+        allowNull: false
     }
 });
 
